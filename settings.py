01f2bd07c46e8f6c5c2a321da99c2e4854ae9107
perodaah <31420445+perodaah@users.noreply.github.com>
2026-01-11 21:36:38 +0100

    Caching in Django

---
+++ settings.py
@@ -0,0 +1,38 @@
+INSTALLED_APPS = [
+    # default apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # custom apps
+    'properties',
+
+    # third-party apps
+    'django_redis',
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'password123',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
